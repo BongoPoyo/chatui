4dc996cef85c2a91245323bbe44cd584c027ff36
Bongo <bongopoyo@proton.me>
2025-12-09 12:50:17 +0500

    UI NOW WORKS

--- ui.py
+++ ui.py
@@ -5,15 +5,18 @@ from textual.containers import VerticalScroll
 from ollama import chat
 
 
-class MarkdownApp(App):
+class ChaTUI(App):
     CSS_PATH = "layout.tcss"
-    BINDINGS = [("q", "quit", "Quit app")]
+    BINDINGS = [("q", "quit", "Quit app"),
+                ("a", "focus_text", "Focus Text Field")
+                ]
+    full_text = ""
 
     md_viewer: Markdown
     input_prompt: Input
 
     def compose(self) -> ComposeResult:
-        yield Header()
+        yield Header("CHATUI")
         self.md_viewer = Markdown("# Chat output will appear here.")
         self.md_viewer.code_indent_guides = True
         self.input_prompt = Input(placeholder="Enter the question")
@@ -25,32 +28,33 @@ class MarkdownApp(App):
     def on_mount(self) -> None:
         self.theme = "catppuccin-mocha"
 
+    def action_focus_text(self) -> None:
+        self.md_viewer.blur()
+        self.input_prompt.focus()
+
     async def on_input_submitted(self, event: Input.Submitted) -> None:
         prompt = self.input_prompt.value
         self.input_prompt.value = ""
+        event.input.blur()
+
         self.md_viewer.focus()
         self.run_worker(self.run_llm_stream(prompt), exclusive=True)
 
     async def run_llm_stream(self, prompt: str):
 
-        await self.md_viewer.update("Generating your answer.")
-
         stream = chat(
             model="gemma3:1b",
             messages=[{"role": "user", "content": prompt}],
-            stream=True
+            stream=True,
         )
-        # Buffer for all markdown received so far
-        full_text = ""
-        # Stream chunks from Ollama
+        self.full_text += "\n\n # Question: " + prompt + "\n\n"
         for chunk in stream:
             token = chunk["message"]["content"]
-            full_text += token
-
-        # Update markdown live
-        await self.md_viewer.update(full_text)
+            self.full_text += token
+            # Update markdown live
+            await self.md_viewer.update(self.full_text)
 
 
 if __name__ == "__main__":
-    app = MarkdownApp()
+    app = ChaTUI()
     app.run()
